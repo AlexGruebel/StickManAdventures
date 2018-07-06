e31137b3fd30f6ffcf9504ed36877d125e7988b1
AlexGruebel <alexander-gruembel@online.de>
2018-07-06 10:30:44 +0200

    removed old code

--- StickManAdventure/Program.cs
+++ StickManAdventure/Program.cs
@@ -1,50 +1,33 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
+using Newtonsoft.Json;
+using StickManAdventure.Desktop.GUI;
 using StickManAdventureLib;
+
 namespace StickManAdventure.Desktop
 {
-    /// <summary>
-    /// The main class.
-    /// </summary>
     public static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-			LevelConfig config = new LevelConfig();
 
-			int[,] map = new int[,] {
-				{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,1,1,1,0,0,0,1,10,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-				{1,1,0,1,1,1,0,1,1,1,1,0,0,0,1,1,1,1,0,0,1,1,1,1,1,0,1,1,1,1},
-				{1,1,1,1,0,1,1,1,1,1,1,0,0,0,1,1,1,1,0,0,1,1,1,1,1,0,1,1,1,1},
-				{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-			};
-            
-			config.map = new Map(ref map);
-
-			config.player = new PlayerObject(new Vector2(0,0),10, "Alex")
+		[STAThread]
+		static void Main()
+		{
+			try
 			{
-				contentName = "StickmanSingle",
-				//SizeToWindow = 10,
-				Rectangle = new Microsoft.Xna.Framework.Rectangle(0, 0, 0, 0),
-				SizeRelativeToWindowHeightInPerc = 17,
-                
-			};
-            
-			using (var level = new Level(config))
-			{
-				level.Run();
+				LevelConfig config = JsonConvert.DeserializeObject<LevelConfig>(File.ReadAllText(Path.Combine("Levels", "Level1.json")));            
+				using (var level = new Level(config))
+				{
+					level.Run();
+				}
 			}
+			catch (Exception)
+			{
+				
+			}
+                     
         }
     }
 }
